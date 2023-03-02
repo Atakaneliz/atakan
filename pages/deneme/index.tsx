7e2eec8ef6ef7d257d2a59ab2f4eeeed3a6eb8ef
Atakan Ev <atakaneliz@outlook.com>
2023-03-02 18:57:25 +0300

    hadi bakalım

--- pages/deneme/index.tsx
+++ pages/deneme/index.tsx
@@ -4,28 +4,24 @@ export default function Deneme() {
     return (
         <div className='flex flex-col w-full h-full min-h-screen items-center justify-center'>
             <h1 className='text-4xl font-bold'>Deneme</h1>
-            <iframe
-                src="https://player.twitch.tv/?channel=purplebixi&parent=www.atakaneliz.com"
-                height="720"
-                width="1280"
-                frameBorder="0"
-                scrolling="no"
-                allowFullScreen={true}>
-            </iframe>
-            <iframe
-                src="https://player.twitch.tv/?channel=purplebixi&parent=atakaneliz"
-                height="720"
-                width="1280"
-                frameBorder="0"
-                scrolling="no"
-                allowFullScreen={true}>
-            </iframe>
-            <iframe
-                src="https://www.youtube.com/embed/SqHIO2zhxbA"
-                height="720"
-                width="1280"
-                allowFullScreen>
-            </iframe>
+            <div className='w-full h-full grid grid-cols-2'>
+                <iframe
+                    src="https://player.twitch.tv/?channel=purplebixi&parent=www.atakaneliz.com"
+                    height="720"
+                    width="1280"
+                    frameBorder="0"
+                    scrolling="no"
+                    allowFullScreen={true}>
+                </iframe>
+                <iframe
+                    src="https://player.twitch.tv/?channel=jahrein&parent=www.atakaneliz.com"
+                    height="720"
+                    width="1280"
+                    frameBorder="0"
+                    scrolling="no"
+                    allowFullScreen={true}>
+                </iframe>
+            </div>
         </div>
     )
 }
